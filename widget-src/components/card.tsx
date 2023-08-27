6b35b9482bac1f8d2f94558b8d541a913840d7b6
pixbs <hey@pixbs.com>
2023-08-27 20:04:20 +0200

    useStickable test

--- widget-src/components/card.tsx
+++ widget-src/components/card.tsx
@@ -1,2 +1,15 @@
-const { widget } = figma
-const { useEffect, useSyncedMap, useSyncedState, Text, AutoLayout, SVG } = widget
+const { AutoLayout, Text } = widget
+
+export interface CardProps {
+    name: string
+}
+
+const Card = ({ card } : { card : CardProps }) => {
+    return (
+        <AutoLayout>
+            <Text>{card.name}</Text>
+        </AutoLayout>
+    )
+}
+
+export default Card;
--- widget-src/index.tsx
+++ widget-src/index.tsx
@@ -1,10 +1,59 @@
 const { widget } = figma
-const { useEffect, useSyncedMap, useSyncedState, Text, AutoLayout, SVG } = widget
+const { useSyncedState ,AutoLayout, Text, useWidgetNodeId, useStickableHost, useStickable} = widget
+
 
 const root = figma.root
 
-function App() {
-    return <Column name="Progress" id="3e2c3845-9d11-4ec3-9c01-0c7caa6b3dc4"/>
+
+
+function Widget() {
+    
+    const [parent] = useSyncedState('parent', true)
+    return (
+        parent ? <Column name="ToDo"/> : <Card />
+    )
+
 }
 
-widget.register(App)
+const Column = ({name} : {name:string}) => {
+
+    useStickableHost();
+    const widgetID = useWidgetNodeId();
+
+    return (
+        <AutoLayout
+        height={500}
+        width={300}
+        padding={8}
+        fill="#E6E6E6"
+        >
+            <Text width='fill-parent'>{name}</Text>
+            <Text
+            onClick={() => {
+                const widget = figma.getNodeById(widgetID) as WidgetNode
+                const child = widget.cloneWidget({["parent"] : false})
+                child.x += 100
+            }}
+            >
+                +
+            </Text>
+        </AutoLayout>
+    )
+}
+
+const Card = () => {
+
+    useStickable();
+    return (
+        <AutoLayout
+        height={100}
+        width={200}
+        padding={8}
+        fill="#FFF"
+        >
+            <Text width='fill-parent'>Card</Text>
+        </AutoLayout>
+    )
+}
+
+widget.register(Widget)
