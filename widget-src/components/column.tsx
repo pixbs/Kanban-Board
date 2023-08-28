b456753ffcb281459963f0b03f5b67337703c36a
pixbs <hey@pixbs.com>
2023-08-28 02:42:55 +0200

    base CRUD functions

--- widget-src/components/column.tsx
+++ widget-src/components/column.tsx
@@ -1,22 +1,32 @@
 const { widget } = figma
 const { AutoLayout, Text } = widget
-import Card, {CardProps} from './card'
-
-export interface ColumnProps {
-    name: string
-    cards: CardProps[]
-}
+import Card from './card'
+import { CardProps, ColumnProps } from "../interfaces/props"
 
 const Column = (column: ColumnProps) => {
 
+    const counter = column.cards?.length
+
     return (
         <AutoLayout
+        key={column.name}
         direction='vertical'
         spacing={2}
         >
-            <Text fill="#FFF">{column.name}</Text>
+            <AutoLayout width='fill-parent'>
+                <Text fill="#FFF" width='fill-parent'>{column.name}</Text>
+                <Text 
+                    fill='#FFF'
+                    onClick={() => {
+                        if(!column.onAdd) return
+                        column.onAdd(column, {id: '',name: `Card ${counter + 1}`})
+                    }}
+                >
+                    +
+                </Text>
+            </AutoLayout>
             {column.cards.map((card) => (
-                <Card {...card}/>
+                <Card id={card.id} name={card.name} onChange={column.onChange}/>
             ))}
         </AutoLayout>
     )
